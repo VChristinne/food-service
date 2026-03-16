7d9d794722502adaaf17972b38dab6951e949bdc
Christinne <vanessa0christinne@gmail.com>
2026-03-16 15:16:12 +0100

    refact(api): ClientService for client operations

--- Client/client_routers.py
+++ Client/client_routers.py
@@ -1,27 +1,22 @@
-from fastapi import APIRouter, status
+from typing import Sequence
+from fastapi import APIRouter, Depends, status
+from sqlmodel import Session
 
-from Client.client import ClientSchema
-from Client.client_data import clients
-from Utils.address import fetch_address
+from Database.db_config import db
+from Client.client import ClientSchema, ClientModel
+from Client.client_service import ClientService
 
 router = APIRouter()
 
 
-@router.get("/", status_code=status.HTTP_200_OK)
-async def get_clients() -> list:
-    return clients
+def get_client_service(session: Session = Depends(db.get_session)) -> ClientService:
+    return ClientService(session)
 
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_clients(service: ClientService = Depends(get_client_service)) -> Sequence[ClientModel]:
+    return await service.get_clients()
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_client(client_data: ClientSchema) -> dict:
-    address = await fetch_address(client_data.cep)
-    address["complement"] = client_data.complement
-
-    new_client = {
-        "name": client_data.name,
-        "email": client_data.email,
-        "phone": client_data.phone,
-        "address": address,
-    }
-    clients.append(new_client)
-    return {"message": "Client created successfully", "client": new_client}
+async def create_client(client_data: ClientSchema, service: ClientService = Depends(get_client_service)) -> dict:
+    client = await service.create_client(client_data)
+    return {"message": "Client created successfully", "client": client}
