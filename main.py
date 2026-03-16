3c55bf231789dfa29301e7cbd40d86421a053d9b
Christinne <vanessa0christinne@gmail.com>
2026-03-16 15:16:36 +0100

    feat(api): initialize database tables on app startup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from Client.client_routers import router as client_router
 from Catalogue.catalogue_routers import router as catalogue_router
+from Database.db_config import db
 
 
 version = "v1"
@@ -10,6 +11,8 @@ app = FastAPI(
     description="A RESTful API for managing a food service application.",
     version=version,
 )
+
+db.create_tables()
 
 app.include_router(client_router, prefix=f"/api/{version}/clients", tags=["clients"])
 # app.include_router(employee_router, prefix=f"/api/{version}/employees", tags=["employees"])
