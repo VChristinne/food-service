1d059fa26823236b09f392e9e2a4bf4e575c681e
Christinne <vanessa0christinne@gmail.com>
2026-03-16 11:29:24 +0100

    chore(api): cleaner routing

--- Catalogue/catalogue_routers.py
+++ Catalogue/catalogue_routers.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, HTTPException, status
-from typing import List
-from catalogue_data import catalogue
-from catalogue_schema import CatalogueSchema, DishUpdateModel
+
+from Catalogue.catalogue_data import catalogue
+from Catalogue.catalogue import CatalogueSchema, DishUpdateModel
 
 router = APIRouter()
 
 
-@router.get("/catalogue", response_model=List[CatalogueSchema])
-async def get_catalogue():
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_catalogue() -> list:
     return catalogue
 
 
-@router.get("/catalogue/{dish_id}")
+@router.get("/{dish_id}")
 async def get_dish(dish_id: int) -> dict:
     for dish in catalogue:
         if dish["id"] == dish_id:
@@ -19,14 +19,14 @@ async def get_dish(dish_id: int) -> dict:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Dish not found")
 
 
-@router.post("/catalogue", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_dish(dish_data: CatalogueSchema) -> dict:
     new_dish = dish_data.model_dump()
     catalogue.append(new_dish)
     return new_dish
 
 
-@router.patch("/catalogue/{dish_id}")
+@router.patch("/{dish_id}")
 async def update_dish(dish_id: int, dish_update_data: DishUpdateModel) -> dict:
     for dish in catalogue:
         if dish["id"] == dish_id:
@@ -35,7 +35,7 @@ async def update_dish(dish_id: int, dish_update_data: DishUpdateModel) -> dict:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Dish not found")
 
 
-@router.delete("/catalogue/{dish_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{dish_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_dish(dish_id: int) -> None:
     for dish in catalogue:
         if dish["id"] == dish_id:
