65d3d9775820bef135652cd46b037a70c8b85015
Christinne <vanessa0christinne@gmail.com>
2026-03-16 15:13:54 +0100

    feat(api): add client data management

---
+++ Client/client_repository.py
@@ -0,0 +1,21 @@
+from typing import Any, Sequence
+from sqlmodel import Session, select
+
+from Client.client import ClientModel
+
+
+class ClientRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def create(self, client: ClientModel) -> ClientModel:
+        self.session.add(client)
+        self.session.commit()
+        self.session.refresh(client)
+        return client
+
+    def get_all(self) -> Sequence[Any]:
+        return self.session.exec(select(ClientModel)).all()
+
+    def get_by_id(self, client_id: str) -> ClientModel | None:
+        return self.session.get(ClientModel, client_id)
