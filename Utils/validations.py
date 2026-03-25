016eea6920884398ab34a54ede7077fa2b3ed35d
Christinne <vanessa0christinne@gmail.com>
2026-03-25 20:05:30 +0100

    feat(auth): add auth router and password verification function

--- Utils/validations.py
+++ Utils/validations.py
@@ -4,3 +4,6 @@ from passlib.hash import sha256_crypt
 def hash_password(password: str) -> str:
     hashed = sha256_crypt.using(rounds=1000).hash(password)
     return hashed
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return sha256_crypt.verify(plain_password, hashed_password)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Database.db_config import db
 from Client.client_routers import router as client_router
 from Inventory.inventory_routers import router as inventory_router
 from Catalogue.catalogue_routers import router as catalogue_router
+from Auth.auth_routers import router as auth_router
 
 
 version = "v1"
@@ -19,3 +20,4 @@ db.create_tables()
 app.include_router(client_router, prefix=f"/api/{version}/clients", tags=["clients"])
 app.include_router(inventory_router, prefix=f"/api/{version}/inventory", tags=["inventory"])
 app.include_router(catalogue_router, prefix=f"/api/{version}/catalogue", tags=["catalogue"])
+app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
