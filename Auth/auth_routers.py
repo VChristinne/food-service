00de05bf295ca64d3f7199d8bf91d7b51ade9a59
Christinne <vanessa0christinne@gmail.com>
2026-04-02 21:02:52 +0200

    fix(auth): remove nonsense dependency

--- Auth/auth_routers.py
+++ Auth/auth_routers.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 
 from Database.db_config import db
 from Auth.auth_service import AuthService
-from Auth.auth import get_current_user
 
 router = APIRouter()
 
@@ -22,6 +21,5 @@ def get_auth_service(session: Session = Depends(db.get_session)) -> AuthService:
 async def login(
         credentials: LoginSchema,
         service: AuthService = Depends(get_auth_service),
-        current_user: dict = Depends(get_current_user)
 ) -> dict:
-    return await service.login(credentials.email, credentials.password, current_user["sub"])  # noqa
+    return await service.login(credentials.email, credentials.password)
