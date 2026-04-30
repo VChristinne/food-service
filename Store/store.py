60a7d4cd33e12349e803f8301f968ac9952f05e9
Christinne <vanessa0christinne@gmail.com>
2026-04-30 22:46:45 +0200

    feat(store): add Store model and schemas for creation and updates

---
+++ Store/store.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, JSON
+from sqlmodel import SQLModel, Field
+from pydantic import BaseModel
+from uuid_extensions import uuid7
+from typing import Optional
+from time import time
+
+
+class StoreSchema(BaseModel):
+    phone: str
+    cep: str
+    complement: Optional[str] = None
+
+
+class StoreUpdateSchema(BaseModel):
+    phone: Optional[str] = None
+    cep: Optional[str] = None
+    complement: Optional[str] = None
+
+
+class StoreModel(SQLModel, table=True):
+    __tablename__ = "stores"
+
+    id: str = Field(primary_key=True, default_factory=lambda: str(uuid7()))
+    phone: str
+    address: dict = Field(sa_column=Column(JSON))
+    created_at: int = Field(default_factory=lambda: int(time()))
+    updated_at: int = Field(default_factory=lambda: int(time()))
+    deleted_at: Optional[int] = None
